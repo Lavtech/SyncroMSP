e49c694b3e6123ec536249b4a1d8dbf4946bc027
Lavtech <john@lavtechcomputers.com>
2024-05-06 16:48:38 -0700

    .

--- RMM/Maintenance/WIN/Schedule_a_restart_at_3AM.ps1
+++ RMM/Maintenance/WIN/Schedule_a_restart_at_3AM.ps1
@@ -1,7 +1,7 @@
 # Create a Scheduled Task to run once 
 # Specify the account to run the script
-$User= "NT AUTHORITY\SYSTEM" 
-$taskAction = New-ScheduledTaskAction `
+$User= 'NT AUTHORITY\SYSTEM' 
+$taskAction = 'New-ScheduledTaskAction'
     -Execute 'shutdown' `
     -Argument '-r -t 120'
 $taskAction
@@ -14,7 +14,7 @@ $tasktrigger
 
 # Register the new PowerShell scheduled task
 # The name of your scheduled task.
-$taskName = "ltc.ScheduledRestart"
+$taskName = "ScheduledRestart"
 
 
 # Special fix.  Wouldn't overwrite a task of the same name
