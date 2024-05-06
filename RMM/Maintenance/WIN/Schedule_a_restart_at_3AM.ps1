babf4e39115be92a1b37dc119ee6f945079d920a
Lavtech <john@lavtechcomputers.com>
2024-05-06 16:39:27 -0700

    .

---
+++ RMM/Maintenance/WIN/Schedule_a_restart_at_3AM.ps1
@@ -0,0 +1,34 @@
+# Create a Scheduled Task to run once 
+Import-Module $env:SyncroModule
+# Specify the account to run the script
+$User= "NT AUTHORITY\SYSTEM" 
+$taskAction = New-ScheduledTaskAction `
+    -Execute 'shutdown' `
+    -Argument '-r -t 120'
+$taskAction
+
+# Describe the scheduled task.
+$description = "Scheduled Restart at 3am once"
+
+$taskTrigger = New-ScheduledTaskTrigger -Once -At 3AM
+$tasktrigger
+
+# Register the new PowerShell scheduled task
+# The name of your scheduled task.
+$taskName = "ScheduledRestart"
+
+
+# Special fix.  Wouldn't overwrite a task of the same name
+# So the old copy has to be removed first
+if(Get-ScheduledTask $taskName -ErrorAction Ignore) {
+    Log-Activity -Message "Removing previous copy of command" -EventName "Task Already Exists"
+    Unregister-ScheduledTask -Taskname $taskName -Confirm:$false
+    }
+
+# Register the scheduled task
+Register-ScheduledTask `
+    -User $User `
+    -TaskName $taskName `
+    -Action $taskAction `
+    -Trigger $taskTrigger `
+    -Description $description
