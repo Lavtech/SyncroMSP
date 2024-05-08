c93aacedb10db9b28824548353326c4d907e1bb7
Lavtech <john@lavtechcomputers.com>
2024-05-08 15:58:35 -0700

    .

--- RMM/Monitors/WIN/Computer_age-amd.ps1
+++ RMM/Monitors/WIN/Computer_age-amd.ps1
@@ -1,10 +1,13 @@
 # Fetch the CPU list from GitHub
 $response = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/Lavtech/SyncroMSP/main/RMM/cpu_list"
+# Convert from JSON if not automatically parsed
+if ($response -is [String]) {
+    $response = $response | ConvertFrom-Json
+}
 
-# Assuming $response already contains the structured data
 # Example of accessing data
 foreach ($cpu in $response.AMD) {
-    Write-Host "Model: $($cpu.Model), Architecture: $($cpu.Architecture), Year: $($cpu.Year)"
+    Write-Host "Model: $($cpu.Model), Year: $($cpu.Year)"
 }
 
 foreach ($cpu in $response.Intel) {
