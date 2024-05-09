ac14c9054db4d1096794d387aac146399668d9c6
Lavtech <john@lavtechcomputers.com>
2024-05-08 19:37:51 -0700

    .

--- RMM/Settings/WIN/Retrieve_Windows_Product_Key1.ps1
+++ RMM/Settings/WIN/Retrieve_Windows_Product_Key1.ps1
@@ -25,6 +25,3 @@ for ($i = 24; $i -ge 0; $i--) {
 
 # Output the product key
 Write-Output "Windows Product Key: $productKey"
-
-# This can write to your Asset Custom Fields. Use it to store adhoc information that isn't currently surfaced.
-Set-Asset-Field -Name "Windows_Key" -Value $key
